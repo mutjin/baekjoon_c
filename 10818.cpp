b51320048448b7b8b4ddc2c1131272928deb924b
mutjin <104980932+mutjin@users.noreply.github.com>
2022-05-26 00:22:01 +0900

    Add files via upload

---
+++ 10818.cpp
@@ -0,0 +1,38 @@
+/*
+문제
+N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 
+둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 
+모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+출력
+첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+*/
+
+#include <stdio.h>
+int main(void){
+    int n;
+    scanf("%d",&n);
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    int max=arr[0],min=arr[0];
+    
+    for(int i=0;i<n;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+        if(min>arr[i]){
+            min=arr[i];
+        }
+    }
+    
+    printf("%d %d",min,max);
+    
+}
